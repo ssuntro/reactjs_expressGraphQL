e9837826efb168efd9fde2093303bbce29a859a1
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-07-22 09:27:19 +0700

    add simple query to getting an user

---
+++ schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require('graphql')
+const _ = require('lodash')
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
+
+const users = [
+  { id: '11', firstName: 'Bill', age: 20 },
+  { id: '22', firstName: 'Anne', age: 29 },
+]
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    userJa: {
+      //
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id }) //can return raw json or js obj
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({ query: RootQuery })
+
+//graphQL = 50% query + 50% schema
